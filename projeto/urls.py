39c73d64969c6e8f24cc839ea085b0e09efb6416
Nikolas de Almeida <nikolas.de.almeida.26@gmail.com>
2023-02-01 22:37:13 -0300

    repassando as outras funções que haviam faltado.

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,27 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path
 
-
-# HTTP REQUEST
-def home(request):
-    # 'Respondendo' a "request" do usuário
-    return HttpResponse('HOME')
-
-
-# HTTP REQUEST
-def sobre(request):
-    # 'Respondendo' a "request" do usuário
-    return HttpResponse('Sobre')
-
-
-# HTTP REQUEST
-def contato(request):
-    # 'Respondendo' a "request" do usuário
-    return HttpResponse('Contato')
-
+from recipes.views import contato, home, sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# HTTP REQUEST
+def home(request):
+    # 'Respondendo' a "request" do usuário
+    return HttpResponse('HOME dentro da app')
+
+
+# HTTP REQUEST
+def sobre(request):
+    return HttpResponse('Sobre dentro da app')
+
+
+# HTTP REQUEST
+def contato(request):
+    return HttpResponse('Contato dentro da app')
