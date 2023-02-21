d9ba00b89ff5368a63fa3d091395d4eea4afaf7b
Nikolas de Almeida <nikolas.de.almeida.26@gmail.com>
2023-02-20 21:30:10 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,13 +7,3 @@ from django.shortcuts import render
 def home(request):
     # 'Respondendo' a "request" do usuário
     return render(request, 'recipes/home.html')
-
-
-# HTTP REQUEST
-def sobre(request):
-    return HttpResponse('Sobre dentro da app')
-
-
-# HTTP REQUEST
-def contato(request):
-    return HttpResponse('Contato dentro da app')
